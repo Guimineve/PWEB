a6318f73478f66ad72e8d2ffbde71660f30327b1
António Santos <antonio97santos@gmail.com>
2025-12-07 19:49:29 +0000

    Configuração inicial: Autenticação, BD e Menu

---
+++ GestaoLoja/Data/Inicializacao.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GestaoLoja.Data
+{
+    public static class Initialization
+    {
+        public static async Task CriaDadosIniciais(
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            //Adicionar as roles default
+            string[] roles = new[] { "Admin", "Gestor", "Cliente" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    IdentityRole roleRole = new IdentityRole(role);
+                    await roleManager.CreateAsync(roleRole);
+                }
+            }
+
+            //Adicionar default user - Admin
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "admin@localhost.com",
+                Email = "admin@localhost.com",
+                Nome = "Administrador",
+                Apelido = "Local",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            //validação se o Id já existe
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null) //Tenta encontrar o email. Se não encontrar atribui a senha default e o role
+                {
+                    await userManager.CreateAsync(defaultUser, "Is3C..00");
+                    await userManager.AddToRoleAsync(defaultUser, "Admin");
+                }
+            }
+            
+        }
+
+    }
+}
--- GestaoLoja/Program.cs
+++ GestaoLoja/Program.cs
@@ -1,15 +1,26 @@
-using GestaoLoja.Components;
+﻿using GestaoLoja.Components;
 using GestaoLoja.Components.Account;
 using GestaoLoja.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); //serve para registar servi�os
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents(
+        //fazer isto apenas para dev. Nunca para produção. Deixar em branco
+        options =>
+        {
+            options.DetailedErrors = true;
+        }
+    )
+    .AddHubOptions( //permite ver erros com mais detalhe
+    options =>
+    {
+        options.EnableDetailedErrors = true;
+    });
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
@@ -17,36 +28,65 @@ builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
 builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//Autentica��o e auturiza��o dos users
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    //estrutura da base de dados. Ver Data e Data.Migrations que contem as bases de dados que d�o para alterar e recuperar se necess�rio
+    .AddRoles<IdentityRole>() //tem de ser o primeiro servi�o a ser registado
+    .AddSignInManager() //logo a seguir tem de ser o SignIn
     .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
+//Need to add for data seeding start
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        await Initialization.CriaDadosIniciais(userManager, roleManager);
+        //Log.Information("Identity User Data Seeding finnished");
+
+    }
+    catch (Exception)
+    {
+        throw;
+    }
+}
+
+//Need to add for Data seeding end
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    //ativa a página de erros em detalhe
+    app.UseDeveloperExceptionPage();
 }
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
